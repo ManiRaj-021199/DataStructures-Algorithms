3b74fc82b99f2d71826587152f3e48bf419f2874
Mani Raj <maniraj.021199@gmail.com>
2023-11-24 11:52:13 +0530

    ADDED: Singly Linked List AddFirst, AddLast and GetFirst, GetLast Methods and Test

---
+++ DSA.DataStructures.Test/TestDataStructures/LinkedList/SinglyLinkedListTest.cs
@@ -0,0 +1,106 @@
+﻿namespace DSA.DataStructures.Test;
+
+[TestClass]
+public class SinglyLinkedListTest
+{
+    #region Tests
+    [TestMethod]
+    public void Test_GetHeadValue()
+    {
+        // Arrange
+        SinglyLinkedList<int> list = new();
+
+        // Act
+        _ = list.AddFirst(1);
+
+        // Assert
+        int nValue = list.GetFirst();
+
+        Assert.AreEqual(1, nValue);
+    }
+
+    [TestMethod]
+    public void Test_GetHeadValue_NullReferenceException()
+    {
+        // Arrange
+        SinglyLinkedList<int> list = new();
+
+        // Act
+
+        // Assert
+        Assert.ThrowsException<NullReferenceException>(() => list.GetFirst());
+    }
+
+    [TestMethod]
+    public void Test_GetTailValue()
+    {
+        // Arrange
+        SinglyLinkedList<int> list = new();
+
+        // Act
+        _ = list.AddFirst(1);
+        _ = list.AddFirst(2);
+
+        // Assert
+        int nValue = list.GetLast();
+
+        Assert.AreEqual(1, nValue);
+    }
+
+    [TestMethod]
+    public void Test_GetTailValue_NullReferenceException()
+    {
+        // Arrange
+        SinglyLinkedList<int> list = new();
+
+        // Act
+
+        // Assert
+        Assert.ThrowsException<NullReferenceException>(() => list.GetLast());
+    }
+
+    [TestMethod]
+    public void Test_AddFirst()
+    {
+        // Arrange
+        SinglyLinkedList<int> list = new();
+
+        // Act
+        _ = list.AddFirst(1);
+
+        // Assert
+        int nValue = list.GetFirst();
+
+        Assert.AreEqual(1, nValue);
+
+        _ = list.AddFirst(2);
+        nValue = list.GetFirst();
+
+        Assert.AreEqual(2, nValue);
+    }
+
+    [TestMethod]
+    public void Test_AddLast()
+    {
+        // Arrange
+        SinglyLinkedList<int> list = new();
+
+        // Act
+        _ = list.AddLast(1);
+        _ = list.AddLast(2);
+
+        // Assert
+        int nValue = list.GetLast();
+
+        Assert.AreEqual(2, nValue);
+
+        _ = list.AddLast(3);
+        nValue = list.GetLast();
+
+        Assert.AreEqual(3, nValue);
+
+        nValue = list.GetFirst();
+        Assert.AreEqual(1, nValue);
+    }
+    #endregion
+}
---
+++ DSA.DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,125 @@
+﻿namespace DSA.DataStructures;
+
+public class SinglyLinkedList<T>
+{
+    #region Properties
+    /// <summary>
+    /// Head of the list.
+    /// </summary>
+    private SinglyLinkedListNode<T>? Head { get; set; }
+    #endregion
+
+    #region Publics
+    /// <summary>
+    ///     Returns a head node content.
+    ///     Time complexity      - O(1)
+    ///     Space complexity     - O(1)
+    /// </summary>
+    /// <returns>Head node content</returns>
+    public T? GetFirst()
+    {
+        ThrowExceptionsHelper<SinglyLinkedListNode<T>>.ThrowNullException(this.Head);
+
+        return this.Head!.Data;
+    }
+
+    public T GetElementByIndex(int nIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    ///     Returns a head node content.
+    ///     Time complexity      - O(n)
+    ///     Space complexity     - O(1)
+    /// </summary>
+    /// <returns>End node content</returns>
+    public T? GetLast()
+    {
+        ThrowExceptionsHelper<SinglyLinkedListNode<T>>.ThrowNullException(this.Head);
+
+        SinglyLinkedListNode<T> nodeTemp = this.Head!;
+
+        while(nodeTemp.Next is not null)
+        {
+            nodeTemp = nodeTemp.Next;
+        }
+
+        return nodeTemp.Data;
+    }
+
+    /// <summary>
+    ///     Adding a new node at the beginning of the list.
+    ///     Time complexity      - O(1)
+    ///     Space complexity     - O(1)
+    /// </summary>
+    /// <param name="data">Content of the new node</param>
+    /// <returns>Newly added node</returns>
+    public SinglyLinkedListNode<T> AddFirst(T data)
+    {
+        SinglyLinkedListNode<T> node = new(data, this.Head);
+        this.Head = node;
+
+        return node;
+    }
+
+    /// <summary>
+    ///     Adding a new node at the end of the list.
+    ///     Time complexity      - O(n)
+    ///     Space complexity     - O(1)
+    /// </summary>
+    /// <param name="data">Content of the new node</param>
+    /// <returns>Newly added node</returns>
+    public SinglyLinkedListNode<T> AddLast(T data)
+    {
+        if(this.Head is null)
+        {
+            return AddFirst(data);
+        }
+
+        SinglyLinkedListNode<T> node = new(data);
+        SinglyLinkedListNode<T> nodeTemp = this.Head;
+
+        while(nodeTemp.Next is not null)
+        {
+            nodeTemp = nodeTemp.Next;
+        }
+
+        nodeTemp.Next = node;
+
+        return node;
+    }
+
+    public bool InsertAt(int nIndex, T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool InsertAfter(T previous, T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RemoveAt(int nIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RemoveAll(T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count()
+    {
+        ThrowExceptionsHelper<SinglyLinkedListNode<T>>.ThrowNullException(this.Head);
+
+        return 0;
+    }
+    #endregion
+}
