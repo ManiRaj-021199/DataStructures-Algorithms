0eacedce7ba70552b44a91295a65d0304c5262c1
Mani Raj <maniraj.021199@gmail.com>
2024-02-22 18:04:22 +0530

    ADDED: Array Based Stack Operations Template

---
+++ DSA.DataStructures.Test/TestDataStructures/Stack/ArrayBasedStackTest.cs
@@ -0,0 +1,39 @@
+﻿namespace DSA.DataStructures.Test;
+
+[TestClass]
+public class ArrayBasedStackTest
+{
+    #region Properties
+    private IArrayBasedStack<int> ArrayBasedStack { get; set; } = null!;
+    #endregion
+
+    #region Initialize and Cleanup
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        this.ArrayBasedStack = new ArrayBasedStack<int>();
+    }
+    #endregion
+
+    #region Tests
+    [TestMethod]
+    public void Test_Push()
+    {
+    }
+
+    [TestMethod]
+    public void Test_Pop()
+    {
+    }
+
+    [TestMethod]
+    public void Test_IsEmpty()
+    {
+    }
+
+    [TestMethod]
+    public void Test_Top()
+    {
+    }
+    #endregion
+}
---
+++ DSA.DataStructures/Stack/ArrayBasedStack/ArrayBasedStack.cs
@@ -0,0 +1,26 @@
+﻿namespace DSA.DataStructures;
+
+public class ArrayBasedStack<T> : IArrayBasedStack<T>
+{
+    #region Publics
+    public void Push(T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Pop()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsEmpty()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Top()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+}
