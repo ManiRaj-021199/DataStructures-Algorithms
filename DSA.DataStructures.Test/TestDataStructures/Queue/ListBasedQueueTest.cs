a367f54d63e49f3a3acbb6c1ad278b0ba28795e3
Mani Raj <87110479+ManiRaj-021199@users.noreply.github.com>
2024-02-24 12:45:49 +0530

    ADDED: List Based Queue Templates and Test

---
+++ DSA.DataStructures.Test/TestDataStructures/Queue/ListBasedQueueTest.cs
@@ -0,0 +1,123 @@
+﻿namespace DSA.DataStructures.Test;
+
+[TestClass]
+public class ListBasedQueueTest
+{
+    #region Properties
+    private IListBasedQueue<int> ListBasedQueue { get; set; } = null!;
+    #endregion
+
+    #region Initialize and Cleanup
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        this.ListBasedQueue = new ListBasedQueue<int>();
+    }
+    #endregion
+
+    #region Tests
+    [TestMethod]
+    public void Test_Enqueue_Success()
+    {
+        // Arrange
+        this.ListBasedQueue.Enqueue(1);
+        this.ListBasedQueue.Enqueue(2);
+        this.ListBasedQueue.Enqueue(3);
+        this.ListBasedQueue.Enqueue(4);
+        this.ListBasedQueue.Enqueue(5);
+        this.ListBasedQueue.Enqueue(6);
+
+        // Act
+        int nFront = this.ListBasedQueue.First();
+        int nLast = this.ListBasedQueue.Last();
+
+        // Assert
+        Assert.AreEqual(1, nFront);
+        Assert.AreEqual(6, nLast);
+    }
+
+    [TestMethod]
+    public void Test_Dequeue_Success() 
+    {
+        // Arrange
+        this.ListBasedQueue.Enqueue(1);
+        this.ListBasedQueue.Enqueue(2);
+        this.ListBasedQueue.Enqueue(3);
+        this.ListBasedQueue.Enqueue(4);
+
+        // Act
+        int nFirst = this.ListBasedQueue.Dequeue();
+        int nLast = this.ListBasedQueue.Last();
+
+        // Assert
+        Assert.AreEqual(1, nFirst);
+        Assert.AreEqual(4, nLast);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void Test_Dequeue_OutOfRangeException() 
+    {
+        // Arrange
+
+        // Act
+        _ = this.ListBasedQueue.Dequeue();
+
+        // Assert
+    }
+
+    [TestMethod]
+    public void Test_Front_Success()
+    {
+        // Arrange
+        this.ListBasedQueue.Enqueue(1);
+        this.ListBasedQueue.Enqueue(2);
+        this.ListBasedQueue.Enqueue(3);
+
+        // Act
+        int nFront = this.ListBasedQueue.First();
+
+        // Assert
+        Assert.AreEqual(1, nFront);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void Test_Front_OutOfRangeException()
+    {
+        // Arrange
+        
+        // Act
+        _ = this.ListBasedQueue.First();
+
+        // Assert
+    }
+
+    [TestMethod]
+    public void Test_Last_Success()
+    {
+        // Arrange
+        this.ListBasedQueue.Enqueue(1);
+        this.ListBasedQueue.Enqueue(2);
+        this.ListBasedQueue.Enqueue(3);
+
+        // Act
+        int nFront = this.ListBasedQueue.Last();
+
+        // Assert
+        Assert.AreEqual(3, nFront);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void Test_Last_OutOfRangeException()
+    {
+        // Arrange
+
+        // Act
+        _ = this.ListBasedQueue.Last();
+
+        // Assert
+    }
+    #endregion
+}
---
+++ DSA.DataStructures/Queue/ListBasedQueue/ListBasedQueue.cs
@@ -0,0 +1,30 @@
+﻿namespace DSA.DataStructures;
+
+public class ListBasedQueue<T> : IListBasedQueue<T>
+{
+    #region Properties
+    private List<T> Queue { get; set; } = null!;
+    #endregion
+
+    #region Publics
+    public void Enqueue(T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Dequeue()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T First()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Last()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+}
