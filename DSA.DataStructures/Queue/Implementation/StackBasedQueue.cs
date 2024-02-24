b12ccf6966481967a70afa0290f4ac86ffd813d7
Mani Raj <87110479+ManiRaj-021199@users.noreply.github.com>
2024-02-24 17:52:27 +0530

    ADDED: Stack Based Queue Operations - Enqueue, Dequeue, First and Last

--- DSA.DataStructures/Queue/Implementation/StackBasedQueue.cs
+++ DSA.DataStructures/Queue/Implementation/StackBasedQueue.cs
@@ -3,34 +3,79 @@
 public class StackBasedQueue<T> : IQueueBase<T>
 {
     #region Properties
-    private ArrayBasedStack<T> Stack1 { get; set; } = null!;
-    private ArrayBasedStack<T> Stack2 { get; set; } = null!;
+    private IStackBase<T> Stack1 { get; set; } = new ArrayBasedStack<T>();
+    private IStackBase<T> Stack2 { get; set; } = new ArrayBasedStack<T>();
     #endregion
 
     #region Publics
     public void Enqueue(T data)
     {
-        throw new NotImplementedException();
+        this.Stack1.Push(data);
     }
 
     public T Dequeue()
     {
-        throw new NotImplementedException();
+        if(this.Stack1.Length() == 0)
+        {
+            throw ThrowExceptionsHelper<T>.ThrowArgumentOutOfRangeException();
+        }
+
+        // Reverse Stack
+        int nLengthStack1 = this.Stack1.Length();
+
+        for(int i = 0; i < nLengthStack1 - 1; i++)
+        {
+            this.Stack2.Push(this.Stack1.Pop());
+        }
+
+        T dataNeedToPop = this.Stack1.Pop();
+        int nLengthStack2 = this.Stack2.Length();
+
+        for (int i = 0; i < nLengthStack2; i++)
+        {
+            this.Stack1.Push(this.Stack2.Pop());
+        }
+
+        return dataNeedToPop;
     }
 
     public T First()
     {
-        throw new NotImplementedException();
+        if (this.Stack1.Length() == 0)
+        {
+            throw ThrowExceptionsHelper<T>.ThrowArgumentOutOfRangeException();
+        }
+
+        T data = default!;
+
+        while(this.Stack1.Length() > 0)
+        {
+            data = this.Stack1.Pop();
+
+            this.Stack2.Push(data);
+        }
+
+        while (this.Stack2.Length() > 0)
+        {
+            this.Stack1.Push(this.Stack2.Pop());
+        }
+
+        return data;
     }
 
     public T Last()
     {
-        throw new NotImplementedException();
+        if (this.Stack1.Length() == 0)
+        {
+            throw ThrowExceptionsHelper<T>.ThrowArgumentOutOfRangeException();
+        }
+
+        return this.Stack1.Top();
     }
 
     public int Length()
     {
-        throw new NotImplementedException();
+        return this.Stack1.Length();
     }
     #endregion
 }
